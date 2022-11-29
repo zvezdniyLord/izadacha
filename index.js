42a8e44303d419f0f750df87d0aba0211c5cf8af
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2022-11-29 14:34:35 +0700

    createCart возвращает объект карточки

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cartAppendPlace = document.querySelector(".carts");
 const date = new Date();
+
 class Cart {
     #root;
     constructor(root) {
@@ -14,12 +15,15 @@ class Cart {
             <p class="cart-date">Дата:${date.getFullYear()}</p>
         </section> 
         `);
+        return {id, author, date};
     }
 }
 
 const cart = new Cart(cartAppendPlace);
 cart.createCart(8849, "Первый автор", date);
 cart.createCart(8850, "Второй автор", date);
+cart.createCart(8851, "Давиденко Александр", date);
+console.log(cart)
 
 function createId(salt) {
     const result = `PON-${Math.random(salt) * 36}`;
